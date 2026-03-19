ed13d2d5228ae455b36232a1536fb9075e49f74d
nikkehtine <27138416+nikkehtine@users.noreply.github.com>
2026-03-19 22:49:27 +0100

    Use Gemini API

--- client/app/routes/home.tsx
+++ client/app/routes/home.tsx
@@ -1,7 +1,7 @@
 import { Welcome } from "../welcome/welcome";
 import type { Route } from "./+types/home";
 
-export function meta({}: Route.MetaArgs) {
+export function meta(_props: Route.MetaArgs) {
   return [
     { title: "brAInstorm" },
     { name: "description", content: "Welcome to brAInstorm!" },
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+from dotenv import load_dotenv
 from flask import Flask
+from google import genai
 
 app = Flask(__name__)
+load_dotenv()
+client = genai.Client()
 
 
 @app.route("/")
 def index():
-    return "<p>Hello from brainstorm!</p>"
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents="Give me some kind of variation of 'Hello, World!'. Reply with only that sentence, no extra text.",
+    )
+    return response.text
 
 
 # Catch-all just in case
