d36d1790c4d1f7d89960e21f05e4eff88dee4d3a
nikkehtine <27138416+nikkehtine@users.noreply.github.com>
2026-03-20 23:51:26 +0100

    Upgrade Vite and dependencies

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,7 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import type { UserConfig } from "vite";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
+export default {
+  plugins: [tailwindcss(), reactRouter()],
+} satisfies UserConfig;
